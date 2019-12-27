3788175fa412d705be259b346e95922dcdc1d2ae
Alex Dillon <6540981+alexdillon@users.noreply.github.com>
2019-12-27 00:39:06 -0600

    Added sample application

---
+++ Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using GroupMeClientApi.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new GroupMeClientApi.GroupMeClient("YOUR_API_TOKEN HERE");
+            
+            await client.GetGroupsAsync();
+            await client.GetChatsAsync();
+
+            foreach (IMessageContainer messageContainer in Enumerable.Concat<IMessageContainer>(client.Groups(), client.Chats()))
+            {
+                Console.WriteLine($"{messageContainer.Name} - Updated {messageContainer.UpdatedAtTime.ToString()}");
+            }
+        }
+    }
+}
+
