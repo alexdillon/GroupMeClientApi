c2b7c317fc604b262fd49f22270dc6481f29e1d5
arvinddraffen <goat1080@gmail.com>
2020-08-12 14:51:44 -0500

    Updated the Sample program with changes bade to Contact and ContactList and replaced previous contacts example with a much simpler version reflecting updates

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -11,44 +11,17 @@ namespace Sample
         static async Task Main(string[] args)
         {
             var client = new GroupMeClientApi.GroupMeClient("YOUR_API_TOKEN HERE");
-            
+
             await client.GetGroupsAsync();
             await client.GetChatsAsync();
-            ICollection<Contact> contacts = await client.GetContactsAsync();
-            int x = contacts.Count;
-            while (x > 0)
-            {
-                var sortedContacts = client.Contacts().OrderByDescending(c => c.CreatedAtIso8601Time);
-                var earliestContact = sortedContacts.First();
-                ICollection<Contact> _contacts = await client.GetContactsAsync(true, earliestContact.CreatedAtIso8601Time);
-                x = _contacts.Count;
-            }
+            await client.GetAllContactsAsync();
 
             foreach (IMessageContainer messageContainer in Enumerable.Concat<IMessageContainer>(client.Groups(), client.Chats()))
             {
                 Console.WriteLine($"{messageContainer.Name} - Updated {messageContainer.UpdatedAtTime.ToString()}");
             }
 
-            Console.WriteLine();
-
-            int i = 1;
-            foreach (Contact contact in client.Contacts())
-            {
-                Console.WriteLine($"{contact.Name} - Updated {contact.UpdatedAtTime.ToString()}");
-                i++;
-            }
-            Console.Write($"You have {i} contacts.");
-
-            Contact contact1 = client.Contacts().Where(c => c.Id == "ID_HERE").First();
-
-            if (!contact1.Blocked)
-            {
-                await contact1.BlockContact();        // blocking a contact of Id "ID_HERE"
-            }
-            else
-            {
-                await contact1.UnblockContact();      // unblocking that contact
-            }
+            Console.Write($"\nYou have {client.Contacts.Count()} contacts.");
         }
     }
 }
